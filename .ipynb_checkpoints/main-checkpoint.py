eeda833e6cf7c4325905f076ca897213aaa2667c
Syed Muhammad Shahid <shahidsherazi761@gmail.com>
2026-07-04 10:29:58 +0500

    first commit: AIML Internship projects with 6 tasks

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,9 @@
+from functions import cal_functions as fun
+
+user_input = int(input("enter first value"))
+user2_input = int(input("Enter second value"))
+
+fun.add(user_input,user2_input)
+fun.sub(user_input,user2_input)
+fun.div(user_input,user2_input)
+fun.mult(user_input,user2_input)
